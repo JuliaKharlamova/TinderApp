9a82ab471ab48e4d0b6b3a906bb3fd2234a9f579
Julia <kharlamovajulia@icloud.com>
2020-04-12 13:38:52 +1000

    Initial Commit

---
+++ TinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderApp
+//
+//  Created by Юлия Харламова on 12.04.2020.
+//  Copyright © 2020 Юлия Харламова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
